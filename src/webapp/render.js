ddef106dd24b1782a94693dc628e75bd38c0dd85
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2018-12-26 10:12:10 -0800

    Zachary Chandler added ability to look around.

--- src/webapp/render.js
+++ src/webapp/render.js
@@ -1,8 +1,15 @@
 
-render = {}
+render = {
+	scene: null,
+	engine: null,
+	canvas: null,
+	lookSpeed: 0.02,
+	minLookCap: 0.2,
+	maxLookCap: 0.1,
+};
 
 render.closeScene = function() {
-	model.scene.rootNodes.forEach((n) => {
+	render.scene.rootNodes.forEach((n) => {
 		n.setEnabled(false);
 	});
 }
@@ -11,24 +18,38 @@ render.createScene = function(canvas, engine)  {
 	
 	let scene = new BABYLON.Scene(engine);
 	light = new BABYLON.HemisphericLight("sun", new BABYLON.Vector3(0, 800, 0), scene);
-	let camera = new BABYLON.ArcFollowCamera("Camera", Math.PI / 2, Math.PI / 4, 5, null, scene);
-	camera.attachControl(canvas, true);
+	render.camera = new BABYLON.ArcFollowCamera("Camera", Math.PI / 2, Math.PI / 4, 5, null, scene);
+	render.camera.attachControl(canvas, true);
 
-	model.setScene(scene, camera);
+	model.setScene(scene, render.camera);
 	
 	return scene;
 };
 
 render.loadRender = function() {
-	var canvas = document.getElementById("renderCanvas");
-	var engine = new BABYLON.Engine(canvas, true);
-	var scene = render.createScene(canvas, engine);
+	render.canvas = document.getElementById("renderCanvas");
+	render.engine = new BABYLON.Engine(render.canvas, true);
+	render.scene = render.createScene(render.canvas, render.engine);
 	
-	engine.runRenderLoop(function () { 
-		scene.render();
+	render.engine.runRenderLoop(function () { 
+		render.scene.render();
 	});
 	
 	window.addEventListener("resize", function () { 
-		engine.resize();
+		render.engine.resize();
+	});
+	
+	render.canvas.addEventListener("keydown", function(event) {
+		if (event.code == "KeyA") {
+			render.camera.alpha -= Math.PI * render.lookSpeed;
+		} else if (event.code == "KeyD") {
+			render.camera.alpha += Math.PI * render.lookSpeed;
+		} else if (event.code == "KeyW") {
+			render.camera.beta += Math.PI * render.lookSpeed;
+			render.camera.beta = Math.min(Math.PI/2 - render.maxLookCap, render.camera.beta);
+		} else if (event.code == "KeyS") {
+			render.camera.beta -= Math.PI * render.lookSpeed;
+			render.camera.beta = Math.max(render.minLookCap, render.camera.beta);
+		}
 	});
 }
