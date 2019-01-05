8af0e0ebbba753e831084b21e932cb2661f747e2
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-01-05 07:31:11 -0600

    Zachary Chandler added scroll to zoom

--- src/server/websocket/WebSocketServer.java
+++ src/server/websocket/WebSocketServer.java
@@ -2,8 +2,10 @@ package server.websocket;
 
 import java.io.IOException;
 
+import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.grizzly.websockets.WebSocketAddOn;
 import org.glassfish.grizzly.websockets.WebSocketEngine;
 
@@ -18,17 +20,33 @@ public class WebSocketServer extends Server {
 		shutdown();
 		
 		this.s = HttpServer.createSimpleServer("src/webapp/", 9090);
+		this.disableFileCaching();
+		this.registerWebSocketListener();
+		
+		try {
+			this.s.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void registerWebSocketListener() {
 		WebSocketAddOn a = new WebSocketAddOn();		
 		for (NetworkListener l : s.getListeners()) {
 			l.registerAddOn(a);
 		}
 		
 		WebSocketEngine.getEngine().register("", "/connect", new WebSocketClientManager());
-		try {
-			this.s.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	}
+
+	private void disableFileCaching() {
+		s.getServerConfiguration().getHttpHandlersWithMapping().entrySet().stream().forEach((e) -> {
+			HttpHandler h = e.getKey();
+
+			if (h instanceof StaticHttpHandler) {
+				((StaticHttpHandler) h).setFileCacheEnabled(false);
+			}
+		});
 	}
 
 	@Override
--- src/webapp/view/render.js
+++ src/webapp/view/render.js
@@ -6,48 +6,67 @@ render = {
 	lookSpeed: 0.02,
 	minLookCap: 0.2,
 	maxLookCap: 0.1,
+	scrollZoomScalar: 1 / 500,
+	maxCameraDistance: 10,
+	minCameraDistance: 2
 };
 
-render.closeScene = function() {
+render.closeScene = function () {
 	render.scene.rootNodes.forEach((n) => {
 		n.setEnabled(false);
 	});
 }
 
-render.createScene = function(canvas, engine)  {
-	
+render.createScene = function (canvas, engine) {
+
 	let scene = new BABYLON.Scene(engine);
 	light = new BABYLON.HemisphericLight("sun", new BABYLON.Vector3(0, 800, 0), scene);
 	render.camera = new BABYLON.ArcFollowCamera("Camera", Math.PI / 2, Math.PI / 4, 5, null, scene);
 	render.camera.attachControl(canvas, true);
-	
+
 	return scene;
 };
 
-render.loadRender = function() {
+render.loadRender = function () {
 	render.canvas = document.getElementById("renderCanvas");
 	render.engine = new BABYLON.Engine(render.canvas, true);
 	render.scene = render.createScene(render.canvas, render.engine);
-	
-	render.engine.runRenderLoop(function () { 
+
+	render.engine.runRenderLoop(function () {
 		render.scene.render();
 	});
-	
-	window.addEventListener("resize", function () { 
+
+	window.addEventListener("resize", function () {
 		render.engine.resize();
 	});
-	
-	render.canvas.addEventListener("keydown", function(event) {
-		if (event.code == "KeyA") {
-			render.camera.alpha -= Math.PI * render.lookSpeed;
-		} else if (event.code == "KeyD") {
-			render.camera.alpha += Math.PI * render.lookSpeed;
-		} else if (event.code == "KeyW") {
-			render.camera.beta += Math.PI * render.lookSpeed;
-			render.camera.beta = Math.min(Math.PI/2 - render.maxLookCap, render.camera.beta);
-		} else if (event.code == "KeyS") {
-			render.camera.beta -= Math.PI * render.lookSpeed;
-			render.camera.beta = Math.max(render.minLookCap, render.camera.beta);
+
+	render.canvas.addEventListener("keydown", function (event) {
+		switch (event.code) {
+			case "KeyA":
+				render.camera.alpha -= Math.PI * render.lookSpeed;
+				break;
+			case "KeyD":
+				render.camera.alpha += Math.PI * render.lookSpeed;
+				break;
+			case "KeyW":
+				render.camera.beta += Math.PI * render.lookSpeed;
+				render.camera.beta = Math.min(Math.PI / 2 - render.maxLookCap, render.camera.beta);
+				break;
+			case "KeyS":
+				render.camera.beta -= Math.PI * render.lookSpeed;
+				render.camera.beta = Math.max(render.minLookCap, render.camera.beta);
+				break;
 		}
 	});
+
+	render.canvas.addEventListener("wheel", function (event) {
+		render.camera.radius += event.deltaY * render.scrollZoomScalar;
+
+		if (render.camera.radius > render.maxCameraDistance) {
+			render.camera.radius = render.maxCameraDistance;
+		} else if (render.camera.radius < render.minCameraDistance) {
+			render.camera.radius = render.minCameraDistance;
+		}
+	});
+
 }
