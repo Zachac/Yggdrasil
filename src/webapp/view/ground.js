44e0fc97ec206275173cbf9c3745a194dea9f614
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-01-05 08:21:47 -0600

    Zachary Chandler refactored ground.js

--- src/webapp/view/ground.js
+++ src/webapp/view/ground.js
@@ -4,6 +4,87 @@ ground.tiles = {};
 ground.unusedTiles = [];
 ground.tileLookup = {};
 
+ground.tileUpdate = function (tile) {
+	t = ground.tiles[tile.id];
+	if (t == null) {
+		t = ground.newTile(tile.id);
+	}
+
+	ground.updatePosition(t, tile.position.x, tile.position.y, tile.position.z);
+	ground.updateBiome(t, tile.biome);
+	return ground.updateContents(tile, t);
+}
+
+ground.updatePosition = function (tile, x, y, z) {
+	if (tile.position.x != x || tile.y2 != y || tile.position.z != z) {
+		let ys = ground.tileLookup[x];
+		if (!ys) {
+			ys = {};
+			ground.tileLookup[x] = ys;
+		}
+		let zs = ys[y];
+		if (!zs) {
+			zs = {};
+			ys[y] = zs;
+		}
+
+		zs[z] = tile;
+		tile.position.x = x;
+		tile.position.z = z;
+		tile.y2 = y; // actual Yggdrasil value
+	}
+}
+
+ground.updateBiome = function (tile, type) {
+	tile.material = material[type];
+}
+
+ground.updateContents = function (tile, t) {
+	let lostContents = t.contents;
+
+	if (tile.contents) {
+		t.contents = [];
+		tile.contents.forEach((e) => {
+			t.contents.push(entity.update(e, tile));
+		})
+	} else if (t.contents.length > 0) {
+		t.contents = [];
+	}
+
+	if (lostContents.length > 0) {
+		lostContents.forEach((e, i) => {
+			let contains = t.contents.some((en) => {
+				return en.id == e.id;
+			});
+
+			if (contains) {
+				lostContents.splice(i, 1);
+			}
+		});
+	}
+
+	return lostContents;
+}
+
+ground.newTile = function (id) {
+	let result = null;
+
+	if (ground.unusedTiles.length > 0) {
+		result = ground.unusedTiles.pop()
+	} else {
+		result = BABYLON.MeshBuilder.CreatePlane("tile", { size: 1 }, render.scene);
+		result.setParent(ground.parent);
+	}
+
+	result.rotation.x = Math.PI / 2;
+	result.id = id;
+	ground.tiles[id] = result;
+	result.contents = [];
+	result.setEnabled(true);
+
+	return result;
+}
+
 ground.removeTile = function (t) {
 	delete ground.tiles[t.id];
 	ground.unusedTiles.push(t);
@@ -34,88 +115,4 @@ ground.lookup = function (x, y, z) {
 	}
 
 	return null;
-}
-
-ground.tileUpdate = function (tile) {
-	t = ground.tiles[tile.id];
-	console.log("here");
-
-	if (t == null) {
-		t = ground.newTile(tile.id, tile.biome, tile.position.x, tile.position.y, tile.position.z);
-	}
-
-	let lostContents = t.contents;
-
-	if (tile.contents) {
-		t.contents = [];
-		tile.contents.forEach((e) => {
-			t.contents.push(entity.update(e, tile));
-		})
-	} else if (t.contents.length > 0) {
-		t.contents = [];
-	}
-
-	if (lostContents.length > 0) {
-		lostContents.forEach((e, i) => {
-			let contains = t.contents.some((en) => {
-				return en.id == e.id;
-			});
-
-			if (contains) {
-				lostContents.splice(i, 1);
-			}
-		});
-	}
-
-	return lostContents;
-}
-
-ground.clearTiles = function () {
-	throw "Not yet implemented";
-}
-
-ground.newTile = function (id, type, x, y, z) {
-	let result = null;
-
-	if (ground.unusedTiles.length > 0) {
-		result = ground.unusedTiles.pop()
-	} else {
-		result = BABYLON.MeshBuilder.CreatePlane("tile", { size: 1 }, render.scene);
-		result.setParent(ground.parent);
-	}
-
-	result.rotation.x = Math.PI / 2;
-
-	result.id = id;
-	ground.tiles[id] = result;
-
-
-
-	let ys = ground.tileLookup[x];
-
-	if (!ys) {
-		ys = {}
-		ground.tileLookup[x] = ys;
-	}
-
-	let zs = ys[y]
-
-	if (!zs) {
-		zs = {};
-		ys[y] = zs;
-	}
-
-	zs[z] = result;
-
-	result.position.x = x;
-	result.position.z = z;
-	result.y2 = y;  // actual Yggdrasil value
-
-	result.contents = [];
-
-	result.material = material[type];
-
-	result.setEnabled(true);
-
-	return result;
 }
