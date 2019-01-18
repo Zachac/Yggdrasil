ec6d1cb412eca0cbc0634011b991d0cd61a9d2b5
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-01-17 20:17:49 -0600

    Zachary Chandler added fastnoise library

---
+++ src/main/java/fastnoise/Vector2f.java
@@ -0,0 +1,11 @@
+package fastnoise;
+
+public class Vector2f {
+	float x;
+	float y;
+
+	public Vector2f(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+}
---
+++ src/main/java/fastnoise/Vector3f.java
@@ -0,0 +1,13 @@
+package fastnoise;
+
+public class Vector3f {
+	float x;
+	float y;
+	float z;
+
+	public Vector3f(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
