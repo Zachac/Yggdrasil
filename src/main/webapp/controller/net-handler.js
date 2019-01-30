c8b8ab60be1f8094f31867f1a58a48659420a3be
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-01-29 21:09:44 -0600

    Zachary Chandler dismantled front end

--- src/main/webapp/controller/net-handler.js
+++ src/main/webapp/controller/net-handler.js
@@ -2,26 +2,5 @@ net = {}
 
 net.handleUpdate = function (string) {
 	updt = JSON.parse(string)
-	net.translateCoordinates(updt);
 	console.log(updt);
-	updateHandler.handleUpdate(updt);
-}
-
-net.translateCoordinates = function (d) {
-	if (d.entities) {
-		d.entities.forEach((e) => {
-			if (e.position) {
-				let swap = e.position.y;
-				e.position.y = e.position.x;
-				e.position.x = swap;
-
-				swap = e.position.z;
-				e.position.z = e.position.y;
-				e.position.y = swap;
-
-				e.position.x = -e.position.x;
-				e.position.z = -e.position.z;
-			}
-		});
-	}
 }
