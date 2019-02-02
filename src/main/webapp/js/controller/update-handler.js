095eb6642f0a1e1a86f4ee5437280bdc8e58460c
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-02-02 09:58:39 -0600

    Zachary Chandler refactored chunk updates

--- src/main/webapp/js/controller/update-handler.js
+++ src/main/webapp/js/controller/update-handler.js
@@ -4,34 +4,37 @@
 var UpdateHandler = function (canvas) {
     this._canvas = canvas;
     this._tiles = [];
-    console.log(this._canvas);
 }
 
 UpdateHandler.prototype = {
     handle: function (update) {
         if (update.chunk) {
-            let newCanvas = this._canvas.cloneNode(false);
-            this._canvas = this._canvas.parentElement.replaceChild(newCanvas, this._canvas);
-            this._canvas = newCanvas;
-            this._tiles.length = 0;
-
-            let rows = this._tiles;
-            let canvas = this._canvas;
-
-            update.chunk.tiles.forEach(function (e) {
-                let row = [];
-
-                e.forEach(function (biome) {
-                    let tile = Elements.createTile();
-                    tile.appendChild(Elements.createImage(Materials[biome]), tile);
-                    canvas.appendChild(tile);
-                    row.push(tile);
-                })
-
-                canvas.appendChild(Elements.br());
-
-                rows.push(row);
-            })
+            this._chunkUpdate(update.chunk);
         }
+    },
+
+    _chunkUpdate: function (chunk) {
+        this._removeChunk();
+
+        for (let i = 0; i < chunk.tiles.length; i++) {
+            let row = [];
+
+            for (let j = 0; j < chunk.tiles[i].length; j++) {
+                let tile = Elements.createTile();
+                tile.appendChild(Elements.createImage(Materials[chunk.tiles[i][j]]), tile);
+                this._canvas.appendChild(tile);
+                row.push(tile);
+            }
+
+            this._canvas.appendChild(Elements.br());
+            this._tiles.push(row);
+        }
+    },
+
+    _removeChunk: function () {
+        let newCanvas = this._canvas.cloneNode(false);
+        this._canvas = this._canvas.parentElement.replaceChild(newCanvas, this._canvas);
+        this._canvas = newCanvas;
+        this._tiles.length = 0;
     }
 }
