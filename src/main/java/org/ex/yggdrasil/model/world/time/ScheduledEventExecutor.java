041d8068be0089b66b6ee3eb20080aa9b8025e88
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2019-02-04 19:59:14 -0600

    Zachary Chandler added ScheduledEventExecutor.java

--- src/main/java/org/ex/yggdrasil/model/world/time/ScheduledEventExecutor.java
+++ src/main/java/org/ex/yggdrasil/model/world/time/ScheduledEventExecutor.java
@@ -6,9 +6,11 @@ public class ScheduledEventExecutor {
 
 	public static final void execute(PriorityQueue<ScheduledEvent> scheduledEvents) {
 		long curTime = System.currentTimeMillis();
+		ScheduledEvent event = scheduledEvents.peek();
 		
-		while (scheduledEvents.peek().whenToExecute <= curTime) {
-			scheduledEvents.poll().r.run();			
+		while (event != null && event.whenToExecute <= curTime) {
+			scheduledEvents.poll().r.run();
+			event = scheduledEvents.peek();
 		}
 	}
 }
